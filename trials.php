6cc9c5eb7a2bf2c0ae1930cc315636512f35db1c
sarun <saruncr@gmail.com>
2016-08-24 07:34:22 +0530

    seconf  commit

--- trials.php
+++ trials.php
@@ -6,7 +6,7 @@
 
 <p> <span ng-bind="firstName"></span> </p>
 <p>Name : <input type="text" ng-model="name" placeholder="Enter name here"></p>
-<h1>Hello {{name}}</h1>
+<h1>Hello {{name}} .. Nice to meet you {{name}}</h1>
 
 </div>
 
