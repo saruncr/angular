5f5bb94f0ef576c486bdf0825491e2edbac2cc9c
sarun <saruncr@gmail.com>
2016-08-24 07:23:55 +0530

    first commit

---
+++ trials.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<script src= "./js/library/angular.min.js"></script>
+<body>
+<div ng-app="" ng-init="app_name='Keep'">
+
+<p> <span ng-bind="firstName"></span> </p>
+<p>Name : <input type="text" ng-model="name" placeholder="Enter name here"></p>
+<h1>Hello {{name}}</h1>
+
+</div>
+
+</body>
+</html>
